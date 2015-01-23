46f7d56767560a59ff1df1d09efc954ffbc83160
Danieladu <1990512dhy@163.com>
2015-01-23 11:11:51 +0800

    二叉树u

--- Sword_2_offer/src/Q6/binarytree.java
+++ Sword_2_offer/src/Q6/binarytree.java
@@ -7,30 +7,52 @@ public class binarytree {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] A=new int[]{1,2,4,7,3,5,6,8};
+		int[] B=new int[]{4,7,2,1,5,3,8,6};
+		
+		int rootval = rebuildtree(A,B);
+		System.out.println(rootval);
 	}
 	
-	public static BinaryTreeNode rebuildtree(int[] A,int[] B,int s1,int e1,int s2,int e2 )
-	{
+	public static int rebuildtree(int[] A,int[] B)
+	{	
+		
 		int length1 = A.length;
 		int length2 = B.length;
-		if(length1!=length2) return null;
+		if(length1!=length2 || length1==0 || length2==0) return -1;
+		
+		BinaryTreeNode root = solve(A,B,0,length1-1,0,length2-1);
+		return root.val;
+	}
+	public static BinaryTreeNode solve(int[] A,int[] B,int s1,int e1,int s2,int e2 )
+	{
+		
+		if(s1==e1) return new BinaryTreeNode(A[s1]);
 		int numberofleft = 0;
-		for(int i=0;i<length2;i++)
+		int numberofright = 0;
+		for(int i=s2;i<e2+1;i++)
 		{
-			if(A[0]==B[i]) 
+			if(A[s1]==B[i]) 
 			{
-				numberofleft = i;
+				numberofleft = i-s2;
 			}
 		}		
-		//A[0]¾ÍÊÇ¸ù ,ÏÈÐò±éÀú£º×ó±ß1~numberofleft,ÓÒ±ßnumberofleft+1~length1
-		//ÖÐÐò±éÀú£º×ó±ß0~numberoflength-1,ÓÒ±ß numberoflength+1~length2
+		numberofright = e1-s1-numberofleft+s2;
 		
-		BinaryTreeNode root = new BinaryTreeNode(A[0]);
+		//A[0]¾ÍÊÇ¸ù ,ÏÈÐò±éÀú£º×ó±ß1~numberofleft,ÓÒ±ßnumberofleft+1~length1-1
+		//ÖÐÐò±éÀú£º×ó±ß0~numberofleft-1,ÓÒ±ß numberofleft+1~length2-1
 		
-		root.left=rebuildtree(A,B,1,numberofleft,numberofleft+1,length1);
-		root.right=rebuildtree(A,B,0,numberofleft-1,numberofleft+1,length2);
+		BinaryTreeNode root = new BinaryTreeNode(A[s1]);
+		if(numberofleft>0)
+		{
+			root.left=solve(A,B,s1+1,numberofleft+s1,s2,s2+numberofleft-1);
+		}
 		
+		if(numberofright>0)
+		{
+			root.right=solve(A,B,numberofleft+s1+1,e1,s2+numberofleft+1,e2);
+		}
+		return root;
 	}
 
 }
