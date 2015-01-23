a71c4a605e6171dc983285bdeda52c40349615c7
Danieladu <1990512dhy@163.com>
2015-01-23 15:00:22 +0800

    将空格换成%20

--- Sword_2_offer/src/Q4/Replace_blank.java
+++ Sword_2_offer/src/Q4/Replace_blank.java
@@ -14,7 +14,7 @@ public class Replace_blank {
 	{
 	//这道题在c++中好理解，在java中不用这么做，因为java中Stirng是固定的，不能改变。
 		//如果非要做，只能返回一个新的String或者新的char数组，但是这样就违背了C++中在String中移动char字符的原理，所以这题不操作
-		//明白其中的道理即可
+		//明白其中的道理即可  
 	}
 
 }
