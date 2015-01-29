f83a4094dbf194aab21a6fbdd88c761be0ebfd27
Danieladu <1990512dhy@163.com>
2015-01-29 17:57:49 +0800

    正则表达式的匹配。使用递归

--- Sword_2_offer/src/Q53/mainclass.java
+++ Sword_2_offer/src/Q53/mainclass.java
@@ -1,38 +1,29 @@
 package Q53;
 //正则表达式匹配。递归
+//没有遇到*，正常处理
+//遇到*，两种情况。1、*前面相同，则分3步。第一步，继续匹配*后面，第二步，匹配*前面那个数，第三步，不管*和其前面那个数
+//2、*前面不同，则无视*，匹配*后面的
 public class mainclass {
-
-
-	public static boolean solve(String a,String b)
+	public static boolean ismatch(String s,String p)
 	{	
-		int length1 = a.length();
-		int length2 = b.length();
+		return ismatchcore(s,p,0,0); 
 		
-		if(length1==0)
-			return length2==0;
-		 if (length1 == 1) {
-	            if (b.equals(a) || b.equals(".") && a.length() == 1) {
-	                return true;
-	            } else
-	                return false;
-	        }
-		if(b.charAt(1)!='*') 
+	}
+	
+	public static boolean ismatchcore(String s,String p,int indexs,int indexp)
+	{
+		int lengths = s.length();
+		int lengthp = p.length();
+		if(indexs == lengths-1 && indexp==lengthp-1) return true;
+		if(indexs != lengths-1 && indexp==lengthp-1) return false;
+		if(p.charAt(indexp+1)=='*') 
 		{
-				if(a.length()>0 && a.charAt(0)==b.charAt(0) || b.charAt(0)=='.')
-					return solve(a.substring(1),b.substring(1));
-				return false;
+			if(s.charAt(indexs) == p.charAt(indexp) || (p.charAt(indexp)=='.' && indexs!=lengths-1))
+			 return ismatchcore(s,p,indexs+1,indexp+2) || ismatchcore(s,p,indexs,indexp+2) || ismatchcore(s,p,indexs+1,indexp);
+			else return ismatchcore(s,p,indexs,indexp+2);
 		}
-		else
-		{
-			while(a.length()>0 && a.charAt(0)==b.charAt(0) || b.charAt(0)=='.')
-			{
-				if(solve(a,b.substring(2))) return true;
-				a=a.substring(1);
-			}
-			return solve(a,b.substring(2));
-			
-		}
-		
-		
+		if(s.charAt(indexs) == p.charAt(indexp) || (p.charAt(indexp)=='.' && indexs!=lengths-1))
+			return ismatchcore(s,p,indexs+1,indexp+1);
+		return false;
 	}
 }
